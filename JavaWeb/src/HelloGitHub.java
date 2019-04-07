e35c1fa90d1905ee632a3890970757a60d20a48c
Benita <76908977@qq.com>
2019-04-07 12:25:28 +0800

    打印hello

--- JavaWeb/src/HelloGitHub.java
+++ JavaWeb/src/HelloGitHub.java
@@ -1,8 +1,11 @@
-package PACKAGE_NAME;
-
 /**
  * @author bing
  * @create 2019-04-07 10:30
  */
 public class HelloGitHub {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+    }
 }
